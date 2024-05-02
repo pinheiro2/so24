60c0a3e2176e29d1ab4d0e68d8f35bc57e9fc5f9
myagi-ryota <a93285@alunos.uminho.pt>
2024-05-02 17:56:41 +0100

    status implementado

--- src/client.c
+++ src/client.c
@@ -9,10 +9,11 @@
 
 int main (int argc, char * argv[]){
 
+	/*
 	if (argc < 5) {
 		printf("Missing argument.\n");
 		_exit(1);
-	}
+	}*/
 	int fdc, fds;
 	char fifoc_name[30];
 	sprintf(fifoc_name, CLIENT "%d", getpid());
@@ -37,9 +38,60 @@ int main (int argc, char * argv[]){
 		
 		fdc = open(fifoc_name, O_RDONLY);
 		read(fdc, &m, sizeof(m));
-		printf("TASK %d\n", m.id);
+		char line[1024];
+		sprintf(line, "TASK %d Received\n", m.id);
+		write(1, line, strlen(line));
 		close(fdc);
 		
+	}
+
+	if(argc == 2){
+		if(strcmp(argv[1], "status")==0){
+			m.type = 3;
+			m.pid = getpid();
+			fds = open(SERVER, O_WRONLY);
+			write(fds, &m, sizeof(m));
+			close(fds);
+
+
+			int mode = -1; //flag que identifica o momento a qual uma task se refere
+			fdc = open(fifoc_name, O_RDONLY);
+			while(read(fdc, &m, sizeof(m))>0){
+				char line[1024];
+				if(m.type == 4){
+					mode = 4;
+					sprintf(line, "Executing\n");
+				}
+				else{
+					if(m.type == 5){
+						mode = 5;
+						sprintf(line, "\nScheduled\n");
+					}
+					else{
+						if(m.type == 6){
+							mode = 6;
+							sprintf(line, "\nCompleted\n");
+						}
+						else{
+							if (mode!=6){
+								sprintf(line, "%d \"%s\"\n", m.id, m.program);
+							}
+							else{
+								//sprintf(line, "%d \"%s\" %d ms\n", m.id, m.program, m.time);
+								sprintf(line, "%s", m.program);
+							}
+						}
+					}
+				}
+				
+				
+				
+				write(1, line, strlen(line));
+				
+			}
+			close(fdc);
+
+		}
 	}
 	unlink(fifoc_name);
 	
--- src/orchestrator.c
+++ src/orchestrator.c
@@ -16,6 +16,13 @@ int compare(struct msg* a, struct msg* b){
 	return (a->time - b->time);
 }
 
+struct timeval clone_timeval(struct timeval t) {
+    struct timeval clone;
+    clone.tv_sec = t.tv_sec;
+    clone.tv_usec = t.tv_usec;
+    return clone;
+}
+
 struct msg* cloneMsg(struct msg m){
 	struct msg* r = malloc(sizeof(m));
 
@@ -24,6 +31,9 @@ struct msg* cloneMsg(struct msg m){
 	r->time = m.time;
 	r->id = m.id;
 	r->type = m.type;
+	r->start_time = clone_timeval(m.start_time);
+	r->end_time = clone_timeval(m.end_time);
+
 
 	return r;
 }
@@ -45,6 +55,45 @@ long timeElapsed(struct timeval start_time, struct timeval end_time){
 	milliseconds = (end_time.tv_sec - start_time.tv_sec) * 1000 +
                    (end_time.tv_usec - start_time.tv_usec) / 1000;
 	return milliseconds;
+}
+
+void statusTasksHeap(int fdc, BinaryHeap* heap){
+	int n = binary_heap_num_entries(heap);
+	struct msg* auxiliar[n];
+	for(int i = 0; i < n; i++){
+		struct msg* a = binary_heap_pop(heap);
+		write(fdc, a, sizeof(struct msg));
+		auxiliar[i] = a;
+	}
+	for(int j = 0; j < n; j++){
+		binary_heap_insert(heap, auxiliar[j]);
+	}
+}
+
+int readLine(int fd, char* buffer){
+	char aux[2];
+	int i = 0;
+	while(read(fd, aux, 1)>0){
+		buffer[i] = aux[0];
+		if(aux[0]=='\n'){
+			buffer[i+1] = '\0';
+			return 1;
+		}
+		i++;
+	}
+
+	return -1;
+
+}
+
+void statusCompleted(int fdlog, int fdc){
+	char buffer[1024];
+	struct msg m;
+	while(readLine(fdlog, buffer) != -1){
+		strcpy(m.program, buffer);
+		m.type = 1;
+		write(fdc, &m, sizeof(m));
+	}
 }
 
 int main (int argc, char * argv[]){
@@ -106,6 +155,7 @@ int main (int argc, char * argv[]){
 	}
 
 	BinaryHeap* heap = binary_heap_new(BINARY_HEAP_TYPE_MIN, compare);
+	struct msg* exec[nworkers];
 
 	while(1){
 		fds = open(SERVER, O_RDONLY);
@@ -134,6 +184,7 @@ int main (int argc, char * argv[]){
 						fdw = open(fifow_name, O_WRONLY);
 						write(fdw, &m, sizeof(struct msg));
 						close(fdw);
+						exec[w] = cloneMsg(m);
 					}
 					else{
 						struct msg* r = cloneMsg(m);
@@ -146,6 +197,7 @@ int main (int argc, char * argv[]){
 			}
 			if(m.type == 2){
 				printf("Obrigado worker%d por completares a TASK%d!\n", m.pid, m.id);
+				free(exec[m.pid]);
 				if(binary_heap_num_entries(heap)){
 					struct msg* a = binary_heap_pop(heap);
 					char fifow_name[30];
@@ -153,17 +205,49 @@ int main (int argc, char * argv[]){
 					fdw = open(fifow_name, O_WRONLY);
 					write(fdw, a, sizeof(struct msg));
 					close(fdw);
+					exec[m.pid] = cloneMsg(*a);
 				}
 				else{
 					available[m.pid] = 1;
 				}
 
-				char line[30];
+				char line[1024];
 				long realTime = timeElapsed(m.start_time, m.end_time);
-				sprintf(line, "%d %ld\n", m.id, realTime);
+				sprintf(line, "%d \"%s\" %ld ms\n", m.id, m.program, realTime);
 				fdlog = open(LOG, O_CREAT | O_APPEND | O_WRONLY, 0666);
 				write(fdlog, line, strlen(line));
 				close(fdlog);
+			}
+			if(m.type == 3){
+				struct msg t;
+				
+				t.type = 4; //tarefas em execução header
+
+				char fifoc_name[30];
+				sprintf(fifoc_name, CLIENT "%d", m.pid);
+				fdc = open(fifoc_name, O_WRONLY);
+				write(fdc,&t,sizeof(m));
+				
+				//enviar tarefas em execução 
+				for(int j = 0; j < nworkers; j++){
+					if(available[j] != 1){
+						write(fdc, exec[j], sizeof(m));
+					}
+				}
+
+				t.type = 5; //tarefas em fila de espera header
+				write(fdc,&t,sizeof(m));
+
+				statusTasksHeap(fdc, heap);
+
+				t.type = 6; //tarefas terminadas header
+				write(fdc,&t,sizeof(m));
+
+				fdlog = open(LOG, O_CREAT | O_RDONLY, 0666);
+				statusCompleted(fdlog, fdc);
+				close(fdlog);
+
+				close(fdc);
 			}
 		}
 		close(fds);
